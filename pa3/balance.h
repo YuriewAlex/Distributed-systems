9cb9aabd97d720ebb02b51a5113f7e84a61966bd
YuriewAlex <45200321+YuriewAlex@users.noreply.github.com>
2024-06-19 13:19:33 +0300

    Delete pa3 directory

--- pa3/balance.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BALANCE_H_
-#define _BALANCE_H_
-
-#include "proc.h"
-#include "messaging.h"
-
-void add_balance_state_to_history ( BalanceHistory* history, BalanceState state );
-void commit_transaction ( proc_t* proc, Message* msg );
-#endif // _BALANCE_H_
